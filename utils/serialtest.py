9efe9fdf02ad2893dacc7ba5987a886dc557311b
Chris Clonch <chris@theclonchs.com>
2015-03-19 01:02:37 +0000

    Added reset and input flush. Changed to sys.stdout.write to avoid adding additional newline.

--- utils/serialtest.py
+++ utils/serialtest.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python2
 
 import serial
+import sys
+import time
 
-ser = serial.Serial( '/dev/ttyACM0', 9600 )
+arduino = serial.Serial( '/dev/ttyACM0', 115200 )
+
+# Toggle DTR to reset Arduino
+arduino.setDTR(False)
+time.sleep(1)
+# toss any data already received, see
+# http://pyserial.sourceforge.net/pyserial_api.html#serial.Serial.flushInput
+arduino.flushInput()
+arduino.setDTR(True)
+
 while True:
-	print ser.readline( )
+	sys.stdout.write( arduino.readline( ) )
