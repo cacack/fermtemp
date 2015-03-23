a351c80725c3b893a422cea2643eda35170d8bfa
Chris Clonch <chris@theclonchs.com>
2015-03-23 00:25:31 +0000

    Initial gateway python script.

---
+++ gw/gw.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2
+
+from influxdb import InfluxDBClient
+import json
+import serial
+import signal
+import sys
+import time
+
+# SIGINT function.
+def sig_SIGINT( signal, frame ):
+	arduino.close( )
+	print 'Exiting..'
+	sys.exit( 0 )
+# Trap interrupt signal.
+signal.signal( signal.SIGINT, sig_SIGINT )
+
+influxdb = InfluxDBClient('skywalker', 8086, 'root', 'root', 'fermtemp')
+arduino = serial.Serial( '/dev/ttyACM0', 115200 )
+
+print "Reseting Arduino.."
+
+# Toggle DTR to reset Arduino
+arduino.setDTR(False)
+time.sleep(1)
+# toss any data already received, see
+# http://pyserial.sourceforge.net/pyserial_api.html#serial.Serial.flushInput
+arduino.flushInput()
+arduino.setDTR(True)
+
+print "Reading data.."
+
+# Loop forever.
+while True:
+	keys = []
+	values = []
+	# Read a complete line from the serial input.
+	line = arduino.readline( )
+	# Chomp
+	line = line.rstrip( '\n' )
+	# Split line on ',', then split on ':', then loop on resultant key/value pair.
+	for key,value in (item.split(':') for item in line.split( ',' )):
+		#print "%s == %s" % (key, value)
+		keys.append( key )
+		values.append( float( value ) )
+	print "Sending data.."
+	datas = []
+	data = {}
+	data['name'] = 'fermtemp'
+	data['columns'] = keys
+	data['points'] = [values]
+	datas.append( data )
+	jdata = json.dumps(datas)
+	print jdata
+	influxdb.write_points( jdata )
